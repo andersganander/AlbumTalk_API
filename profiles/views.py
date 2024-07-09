23c2a9686df495ea01f8fe89df63d182dfb8ca20
Anders Ganander <anders.ganander@gmail.com>
2024-07-09 08:41:41 +0000

    Add authentication and authorization #9

--- profiles/views.py
+++ profiles/views.py
@@ -4,25 +4,29 @@ from rest_framework import status
 from .models import Profile
 from .serializers import ProfileSerializer
 from django.http import Http404
+from AlbumTalk_API.permissions import IsOwnerOrReadOnly
 
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many= True)
+        serializer = ProfileSerializer(profiles, many= True, context= {'request' : request})
         return Response(serializer.data)
 
 class ProfileDetail(APIView):
     serializer_class = ProfileSerializer
+    permission_classes =[IsOwnerOrReadOnly]
+
     def get_object(self, pk):
         try:
             profile=Profile.objects.get(pk=pk)
+            self.check_object_permissions (self.request, profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
 
     def put (self, request, pk):
         profile =self.get_object(pk)
-        serializer=ProfileSerializer(profile, data=request.data)
+        serializer=ProfileSerializer(profile, data=request.data, context= {'request' : request})
         
         if serializer.is_valid():
             serializer.save()
@@ -32,5 +36,5 @@ class ProfileDetail(APIView):
 
     def get(self, request, pk):
         profile = self.get_object(pk)
-        serializer=ProfileSerializer(profile)
+        serializer=ProfileSerializer(profile, context= {'request' : request})
         return Response(serializer.data)
