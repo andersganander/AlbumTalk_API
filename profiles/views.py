1e5afade07aa2c18a495fab4074acd49b2b2e529
Anders Ganander <anders.ganander@gmail.com>
2024-08-01 14:55:02 +0000

    Add reviews_count to profile serializer

--- profiles/views.py
+++ profiles/views.py
@@ -1,40 +1,43 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from django.db.models import Count
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Profile
 from .serializers import ProfileSerializer
-from django.http import Http404
 from AlbumTalk_API.permissions import IsOwnerOrReadOnly
 
-class ProfileList(APIView):
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many= True, context= {'request' : request})
-        return Response(serializer.data)
-
-class ProfileDetail(APIView):
+class ProfileList(generics.ListAPIView):
+    queryset = Profile.objects.annotate(
+        reviews_count=Count('owner__review', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
-    permission_classes =[IsOwnerOrReadOnly]
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__following__followed__profile',
+        'owner__followed__owner__profile',
+    ]
+    ordering_fields = [
+        'reviews_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
-    def get_object(self, pk):
-        try:
-            profile=Profile.objects.get(pk=pk)
-            self.check_object_permissions (self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile if you're the owner.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.annotate(
+        reviews_count=Count('owner__review', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
+    serializer_class = ProfileSerializer
 
-    def put (self, request, pk):
-        profile =self.get_object(pk)
-        serializer=ProfileSerializer(profile, data=request.data, context= {'request' : request})
-        
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer=ProfileSerializer(profile, context= {'request' : request})
-        return Response(serializer.data)
+   
