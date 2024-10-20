118c021d7afb6f0b19e03c3023a89dee6b57f6bf
Anders Ganander <anders.ganander@gmail.com>
2024-10-20 16:49:53 +0000

    Add favorite id

--- albums/serializers.py
+++ albums/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Album, Genre
+from favorites.models import Favorite
 
 
 class AlbumSerializer(serializers.ModelSerializer):
@@ -13,6 +14,16 @@ class AlbumSerializer(serializers.ModelSerializer):
     # genre = serializers.ReadOnlyField()
     reviews_count = serializers.ReadOnlyField()
     favorite_count = serializers.ReadOnlyField()
+    favorite_id = serializers.SerializerMethodField()
+
+    def get_favorite_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            favorite = Favorite.objects.filter(
+                owner=user, album=obj
+            ).first()
+            return favorite.id if favorite else None
+        return None
 
     class Meta:
         model = Album
@@ -20,5 +31,5 @@ class AlbumSerializer(serializers.ModelSerializer):
             'id', 'title', 'artist', 'image_url', 'release_year',
             'description', 'genre', 'style', 'label', 'album_format',
             'audiodb_idAlbum', 'audiodb_idArtist', 'discogs_id',
-            'wikipedia_id', 'reviews_count', 'favorite_count',
+            'wikipedia_id', 'reviews_count', 'favorite_id', 'favorite_count',
         ]
