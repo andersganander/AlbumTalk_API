4c994108f7bf3ecb6582afe1247cd5614f29c3a1
Anders Ganander <anders.ganander@gmail.com>
2024-11-02 20:47:52 +0000

    add constraint so a user can write only one review for an album #38

--- reviews/models.py
+++ reviews/models.py
@@ -11,7 +11,8 @@ class Review(models.Model):
    
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
      # TODO: This should be a many-to-one relationship with Album model
-    album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='reviews', blank=True, null=True) 
+    # album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='reviews', blank=True, null=True) 
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='reviews') 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     rating = models.SmallIntegerField()
@@ -20,6 +21,7 @@ class Review(models.Model):
 
     class Meta:
         ordering = ['-created_at']
+        unique_together = ('owner', 'album')
 
     def __str__(self):
         return f'{self.owner.username} - {self.id}'
